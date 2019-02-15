6af8006feea46556c15ad0d1cfa902aee2d89703
Anthony Egan <anthony@boulder.swri.edu>
2019-02-15 09:46:48 -0700

    frustrating but functioning ivy-spell checking

--- emacs/config_INITS/spell_INIT.el
+++ emacs/config_INITS/spell_INIT.el
@@ -24,15 +24,62 @@
 ;; for the IVY interface bit
 
 
-(use-package flyspell-correct-ivy
-  :ensure t
-  ;;:bind ("C-M-;" . flyspell-correct-wrapper)
-  :init
-  (setq flyspell-correct-interface #'flyspell-correct-ivy))
+;;(use-package flyspell-correct-ivy
+;;  :ensure t
+;;  :bind ("C-;" . flyspell-correct-wrapper)
+;;  :init
+;;  (setq flyspell-correct-interface #'flyspell-correct-ivy))
+;;
 
 ;; this doesn't work
 ;;(define-key flyspell-mode-map (kbd "C-;") #'flyspell-correct-wrapper)
-(global-set-key (kbd "C-M-;") 'flyspell-buffer)
-(global-set-key (kbd "C-;") 'flyspell-correct-wrapper)
+
+;; these don't work
+;;(global-set-key (kbd "C-M-;") 'flyspell-buffer)
+;;(global-set-key (kbd "C-;") 'flyspell-correct-wrapper)
 
 
+;; Correcting words with flyspell via Ivy
+;; from https://raw.githubusercontent.com/seagle0128/.emacs.d/master/lisp/init-ivy.el
+
+;;(use-package flyspell-correct-ivy
+;;  :ensure t
+;;  :after flyspell
+;;  :bind (:map flyspell-mode-map
+;;              ([remap flyspell-correct-word-before-point] . flyspell-correct-previous-word-generic)))
+;;
+
+;; above isn't quite right
+
+;; I'm seeing the problem. flyspell-correct isn't packaged in MELPA it seems
+
+
+(use-package flyspell-correct
+  :ensure t
+  )
+
+
+;; easy spell check
+;; from https://www.emacswiki.org/emacs/FlySpell
+
+;;(global-set-key (kbd "<f8>") 'ispell-word)
+;;(global-set-key (kbd "C-S-<f8>") 'flyspell-mode)
+;;(global-set-key (kbd "C-M-<f8>") 'flyspell-buffer)
+;;(global-set-key (kbd "C-<f8>") 'flyspell-check-previous-highlighted-word)
+;;(defun flyspell-check-next-highlighted-word ()
+;;  "Custom function to spell check next highlighted word"
+;;  (interactive)
+;;  (flyspell-goto-next-error)
+;;  (ispell-word)
+;;  )
+;;(global-set-key (kbd "M-<f8>") 'flyspell-check-next-highlighted-word)
+
+
+;; OK. This kinda works ... enough to be useful (for now)
+;; toggling only seems to work for flyspell-mode and not flyspell-prog-mode
+;; disabled the prog-mode option
+
+;; HOWTO: hit "F8 o" to toggle on of. Hit f8 twice to fix previous word
+(global-set-key (kbd "<f8> o") 'flyspell-mode)
+;;(global-set-key (kbd "<f8> p") 'flyspell-prog-mode)
+(global-set-key (kbd "<f8> <f8>") 'flyspell-correct-previous)
